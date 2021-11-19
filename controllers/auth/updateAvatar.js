addfb4b6caa343f5974be4557835f1dbab424e29
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2021-11-19 12:57:10 +0200

    error

---
+++ controllers/auth/updateAvatar.js
@@ -0,0 +1,49 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { NotFound, Unauthorized } = require("http-errors");
+const { User } = require("../../model");
+
+const avatarDir = path.join(__dirname, "../../public/avatars");
+
+const updateAvatar = async (req, res, next) => {
+  const { _id, email } = req.user;
+  if (!req.user) {
+    throw new Unauthorized("Not authorized");
+  }
+  if (!req.file) {
+    throw new BadRequest("Enter the file");
+  }
+  const { path: tempDir, originalname } = req.file;
+
+  // const [extension] = originalname.split(".");
+  // console.log(extension);
+  const filename = `${_id}_avatar-image_${originalname}`;
+  const uploadDir = path.join(avatarDir, email, filename);
+
+  try {
+    await fs.rename(tempDir, uploadDir);
+    const avatar = path.join("/avatars", email, filename);
+
+    const data = await User.findByIdAndUpdate(
+      _id,
+      { avatarURL: avatar },
+      { new: true }
+    );
+    if (!data) {
+      throw new NotFound(`User with id=${_id} not found`);
+    }
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      message: "Success update avatar",
+      data: {
+        avatarURL: avatar,
+      },
+    });
+  } catch (error) {
+    await fs.unlink(tempDir);
+    next(error);
+  }
+};
+
+module.exports = updateAvatar;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,13 @@
 const validation = require("./validation");
 const controllerWrapper = require("./controllerWrapper");
 const authenticate = require("./authenticate");
+const sizeJimp = require("./sizeJimp");
+const upload = require("./upload");
 
 module.exports = {
   validation,
   controllerWrapper,
   authenticate,
+  sizeJimp,
+  upload,
 };
